cac10d0ae7ae55792a2a94114bd0310b82f4d29c
rgristle <rgristle@gmail.com>
2014-11-03 21:10:04 +0000

    test 1

---
+++ App1/App1/App1.Windows/tests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit;
+using NUnit.Framework;
+
+namespace App1
+{
+    [NUnit.Framework.TestFixture]
+    class tests
+    {
+        [Test]
+        public void Test1() {
+            Assert.Pass();
+        }
+
+    }
+}
